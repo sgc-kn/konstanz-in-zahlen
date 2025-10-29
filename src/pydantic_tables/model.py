84d589854b8971f42a0e8553f066ee42dc6deca2
Patrik Keller <patrik.keller@konstanz.de>
2025-10-29 08:58:29 +0000

    improve dynamic module import logic

--- src/pydantic_tables/model.py
+++ src/pydantic_tables/model.py
@@ -1,7 +1,10 @@
 import importlib.util
 import pydantic
+import sys
 
+from dataclasses import dataclass
 from pathlib import Path
+from types import ModuleType
 from .table import MetaTable
 from .schema import TableSchema
 
@@ -12,7 +15,64 @@ class ModelLoadError(Exception):
     pass
 
 
-def load(path: Path) -> MetaTable:
+@dataclass(frozen=True)
+class ModelPath:
+    path: Path
+
+    def __post_init__(self):
+        if self.path.suffix != ".md":
+            raise ValueError(
+                f'Model paths must end in ".md". The given path does not: {self.path}'
+            )
+        if len(self.path.parts) < 2:
+            raise ValueError(
+                f"Model must have at least two parts, the dataset and the table. The given path is too short: {self.path}"
+            )
+
+
+def module_name(model: ModelPath) -> str:
+    return f"data.{model.path.parent.stem}.{model.path.stem}"
+
+
+def module_path(model: ModelPath) -> Path:
+    return model.path.with_suffix(".py")
+
+
+def _load_module(model: ModelPath, schema: TableSchema) -> ModuleType:
+    """Helper function for load function (below)"""
+
+    # Dynamically build Python module with Pydantic BaseModel:
+    path = module_path(model)
+
+    try:
+        spec = importlib.util.spec_from_file_location(module_name(model), path)
+        if spec is None or spec.loader is None:
+            raise ModelLoadError(f"Cannot create module spec for {path}")
+
+        # avoid loading module twice
+        if spec.name in sys.modules:
+            return sys.modules[spec.name]
+
+        schema.to_python_file(path)
+
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[spec.name] = module  # required to avoid recursive imports
+
+        try:
+            spec.loader.exec_module(module)
+        except BaseException:
+            # deregister/uncache module if execution fails
+            if sys.modules.get(spec.name) is module:
+                del sys.modules[spec.name]
+            raise
+
+    except Exception as e:
+        raise ModelLoadError(f"Error loading module from {path}: {e}") from e
+
+    return module
+
+
+def load(model: ModelPath | Path) -> MetaTable:
     """Load a Pydantic model from its schema file.
 
     Raises:
@@ -20,24 +80,12 @@ def load(path: Path) -> MetaTable:
                        or Model is not a BaseModel subclass.
     """
 
-    if path.suffix != ".md":
-        raise ValueError(f'Model path must end in ".md": {path}')
+    if isinstance(model, Path):
+        model = ModelPath(model)
 
-    # Dynamically build Python module with Pydantic BaseModel:
-    schema = TableSchema.from_markdown_file(path)
-    path = path.with_suffix(".py")
-    schema.to_python_file(path)
-
-    try:
-        spec = importlib.util.spec_from_file_location(path.stem, path)
-        if spec is None or spec.loader is None:
-            raise ModelLoadError(f"Cannot create module spec for {path}")
-
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-
-    except Exception as e:
-        raise ModelLoadError(f"Error loading module from {path}: {e}") from e
+    path = module_path(model)
+    schema = TableSchema.from_markdown_file(model.path)
+    module = _load_module(model, schema)
 
     if not hasattr(module, "Table"):
         raise ModelLoadError(f"Module {path} does not have a 'Table' attribute")
